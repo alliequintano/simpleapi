f0815866f4920bb8a78775dba09cc757eaa2f1b1
Allie Quintano <alliequintano@gmail.com>
2019-11-12 04:36:26 +0000

    adds test for connection error

--- tests/client/test_client.py
+++ tests/client/test_client.py
@@ -15,3 +15,9 @@ def test_http_error_response(requests_mock):
 
     assert isinstance(error, requests.exceptions.HTTPError)
 
+def test_http_error(requests_mock):
+    requests_mock.get('http://127.0.0.1:5000/current-time', exc=requests.exceptions.ConnectionError)
+    error = get_current_time()
+
+    assert isinstance(error, requests.exceptions.ConnectionError)
+
