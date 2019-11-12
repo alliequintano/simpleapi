77261e3b9abeee01c4dd1ee113b563ca2869dc38
allie <alliequintano@gmail.com>
2019-11-11 18:04:30 -0600

    runs test with pytest but flask error

--- simpleapi/tests/test_api.py
+++ simpleapi/tests/test_api.py
@@ -7,7 +7,7 @@ def app():
     app = create_app()
     return app
 
-def test_get_current_time(client):
-    response = client.get('http://127.0.0.1:5000/current-time')
+def test_get_current_time(app):
+    response = app.get('http://127.0.0.1:5000/current-time')
     assert response.status_code == 200
 
