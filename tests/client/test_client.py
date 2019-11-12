f39ccbb92e32f29494d9bd3651ccb6b74b284a42
Allie Quintano <alliequintano@gmail.com>
2019-11-12 04:05:40 +0000

    fixes fypo in test method

--- tests/client/test_client.py
+++ tests/client/test_client.py
@@ -9,7 +9,7 @@ def test_get_current_time_returns_datetime(requests_mock):
     assert isinstance(current_time, datetime.datetime)
 
 
-def test_connection_error(requests_mock):
+def test_http_error_response(requests_mock):
     requests_mock.get('http://127.0.0.1:5000/current-time', status_code=400)
     error = get_current_time()
 
